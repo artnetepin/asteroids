d941a4182595719cec3efd40842da0467aac02dd
artnetepin <artnetepin@gmail.com>
2025-05-05 09:33:28 +0300

    feat: initialize player

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Initialize the player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +19,13 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        
+        # Update player
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
