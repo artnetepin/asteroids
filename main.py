3b81fe9887664e868add64d947805ad51d1046d2
artnetepin <artnetepin@gmail.com>
2025-05-05 09:55:18 +0300

    update: add groups for player render

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -16,8 +22,11 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        
+        updatables.update(dt)
+
+        for drawable in drawables:
+            drawable.draw(screen)
 
         pygame.display.flip()
 
