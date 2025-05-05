2ee79e0edc2235abd0ac51b8c4006817c70bd56b
artnetepin <artnetepin@gmail.com>
2025-05-05 13:23:16 +0300

    update: shoot to split

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collision_check(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         for drawable in drawables:
